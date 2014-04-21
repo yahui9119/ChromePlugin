227696e0618834c4e11697ff64c4156a446d8837
yahui9119 <yahui9119@qq.com>
2014-04-21 17:52:09 +0800

    0

--- js/options.js
+++ js/options.js
@@ -12,3 +12,13 @@ $('a[name=btndel]').click(function(event) {
 	$('#divkeyvalue').children(':last').remove();
 });	
 });
+function PhoneList($scope) {
+  $scope.phones = [
+    {"name": "Nexus S",
+     "snippet": "Fast just got faster with Nexus S."},
+    {"name": "Motorola XOOM™ with Wi-Fi",
+     "snippet": "The Next, Next Generation tablet."},
+    {"name": "MOTOROLA XOOM™",
+     "snippet": "The Next, Next Generation tablet."}
+  ];
+}
